3beb75b6b3534ea28353c2a6e40d4ec2a176d526
Jen Wachter <jenleighkelly@gmail.com>
2014-08-08 13:18:34 -0400

    Created uploads sync gearman client and worker.

--- main.php
+++ main.php
@@ -6,12 +6,63 @@ Author: johnshopkins
 Version: 0.0
 */
 
+use Secrets\Secret;
+
 class UploadsSyncMain
 {
-  public function __construct()
+  public function __construct($logger, $deps = array())
   {
-    
+    $this->gearmanClient = isset($deps["gearmanClient"]) ? $deps["gearmanClient"] : new \GearmanClient();
+
+    $servers = Secret::get("jhu", ENV, "servers");
+
+    if ($servers) {
+
+      foreach ($servers as $server) {
+        $this->gearmanClient->addServer($server->hostname);
+      }
+
+    } else {
+      $this->logger->addAlert("Servers unavailable for Gearman " . __FILE__ . " on line " + __LINE__);
+    }
+
+    /**
+     * Use this action to hook into when an image
+     * is cropped uisng the crop-thumbnails plugin.
+     * Also catches when an attachment is added.
+     */
+    add_action("wp_update_attachment_metadata", function ($data) {
+      $this->sync("wp_update_attachment_metadata");
+      return $data;
+    });
+
+    // add_action("add_attachment", function () {
+    //   $this->sync("add_attachment WP hook");
+    // });
+
+    add_action("edit_attachment", function () {
+      $this->sync("edit_attachment WP hook");
+    });
+
+    /**
+     * This fires BEFORE WordPress has actually
+     * deleted the file from the server, so rsync
+     * has a chance of missing deleted images
+     * when it runs. The next time it runs, it
+     * shoud catch it.
+     */
+    add_action("delete_attachment", function () {
+      $this->sync("delete_attachment WP hook");
+    });
+
+  }
+
+  public function sync($trigger = null)
+  {
+    return $this->gearmanClient->doBackground("sync_uploads", json_encode(array(
+      "trigger" => $trigger
+    )));
   }
 }
 
-new UploadsSyncMain();
+new UploadsSyncMain($wp_logger);
---
+++ src/Workers/Rsyncer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace UploadsSync\Workers;
+
+class Rsyncer
+{
+  /**
+     * Gearman worker
+     * @var object
+     */
+    protected $worker;
+
+    /**
+     * Monolog
+     * @var object
+     */
+    protected $logger;
+
+    /**
+     * Elasticsearch client
+     * @var object
+     */
+    protected $elasticsearchClient;
+
+    protected $index = "jhu";
+
+    public function __construct($settings = array(), $injection = array())
+    {
+        $this->worker = $settings["worker"];
+        $this->logger = $settings["logger"];
+
+        $this->addFunctions();
+    }
+
+    protected function addFunctions()
+    {
+        $this->worker->addFunction("sync_uploads", array($this, "syncUploads"));
+    }
+    
+    public function syncUploads(\GearmanJob $job)
+    {
+        $workload = json_decode($job->workload());
+        echo $this->getDate() . " Uploads sync triggered from {$workload->trigger}.\n";
+    }
+
+    protected function getDate()
+    {
+        return date("Y-m-d H:i:s");
+    }
+}
