3b7689ecf690b7d9289ff6223159b7fe873aa360
Jen Wachter <jenleighkelly@gmail.com>
2014-08-28 12:32:41 -0400

    Fixed shell_exec() command.

--- src/Workers/Rsyncer.php
+++ src/Workers/Rsyncer.php
@@ -45,7 +45,7 @@ class Rsyncer
         $file = "/home/webuser/wordpress-sync.sh";
         
         if (file_exists($file)) {
-          shell_exec($file);
+          shell_exec("sh {$file}");
         } else {
           $this->logger->addAlert("{$file} is unavailabe. Images are NOT being synced between servers. " . __FILE__ . " on line " + __LINE__);
         }
