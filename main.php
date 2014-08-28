575ff61b79b5a74b355001476189e47a5ff5ce8a
Jen Wachter <jenleighkelly@gmail.com>
2014-08-28 09:34:48 -0400

    Do not add hooks on local or staging.

--- main.php
+++ main.php
@@ -10,39 +10,42 @@ class UploadsSyncMain
 {
   public function __construct($logger, $deps = array())
   {
-    $this->gearmanClient = isset($deps["gearmanClient"]) ? $deps["gearmanClient"] : new \GearmanClient();
+    if (defined("ENV") && (ENV != "local" && ENV != "staging")) {
 
-    // add only the local server (the one wp-admin is locked to)
-    $this->gearmanClient->addServer("127.0.0.1");
+      $this->gearmanClient = isset($deps["gearmanClient"]) ? $deps["gearmanClient"] : new \GearmanClient();
 
-    /**
-     * Use this action to hook into when an image
-     * is cropped uisng the crop-thumbnails plugin.
-     * Also catches when an attachment is added.
-     */
-    add_action("wp_update_attachment_metadata", function ($data) {
-      $this->sync("wp_update_attachment_metadata");
-      return $data;
-    });
+      // add only the local server (the one wp-admin is locked to)
+      $this->gearmanClient->addServer("127.0.0.1");
 
-    // add_action("add_attachment", function () {
-    //   $this->sync("add_attachment WP hook");
-    // });
+      /**
+       * Use this action to hook into when an image
+       * is cropped uisng the crop-thumbnails plugin.
+       * Also catches when an attachment is added.
+       */
+      add_action("wp_update_attachment_metadata", function ($data) {
+        $this->sync("wp_update_attachment_metadata");
+        return $data;
+      });
 
-    add_action("edit_attachment", function () {
-      $this->sync("edit_attachment WP hook");
-    });
+      // add_action("add_attachment", function () {
+      //   $this->sync("add_attachment WP hook");
+      // });
 
-    /**
-     * This fires BEFORE WordPress has actually
-     * deleted the file from the server, so rsync
-     * has a chance of missing deleted images
-     * when it runs. The next time it runs, it
-     * shoud catch it.
-     */
-    add_action("delete_attachment", function () {
-      $this->sync("delete_attachment WP hook");
-    });
+      add_action("edit_attachment", function () {
+        $this->sync("edit_attachment WP hook");
+      });
+
+      /**
+       * This fires BEFORE WordPress has actually
+       * deleted the file from the server, so rsync
+       * has a chance of missing deleted images
+       * when it runs. The next time it runs, it
+       * shoud catch it.
+       */
+      add_action("delete_attachment", function () {
+        $this->sync("delete_attachment WP hook");
+      });
+    }
 
   }
 
