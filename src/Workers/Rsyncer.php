0ae83c9c0fd1c62513c68b3a79aa1b4e80a7ff3b
Jen Wachter <jenleighkelly@gmail.com>
2015-04-06 12:03:08 -0400

    Changed URL

--- src/Workers/Rsyncer.php
+++ src/Workers/Rsyncer.php
@@ -36,19 +36,19 @@ class Rsyncer
     {
         $this->worker->addFunction("sync_uploads", array($this, "syncUploads"));
     }
-    
+
     public function syncUploads(\GearmanJob $job)
     {
         $workload = json_decode($job->workload());
         echo $this->getDate() . " Uploads sync triggered from {$workload->trigger}.\n";
 
-        $file = "/var/www/sites/jhu/current/public/assets/uploadstrigger.txt";
+        $file = "/var/www/sites/jhu/current/public/assets/uploads/trigger.txt";
         $success = file_put_contents($file, "sync");
 
         if ($success === false) {
           $this->logger->addCritical("{$file} could not be written. Images are NOT being synced between servers. " . __FILE__ . " on line " . __LINE__);
         }
-        
+
     }
 
     protected function getDate()
