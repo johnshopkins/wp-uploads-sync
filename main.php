39d3b9fa5b1d50ab79ddeff9daa6c168e70bb0bb
Jen Wachter <jenleighkelly@gmail.com>
2016-08-24 09:04:16 -0400

    Changed delete job to a background job

--- main.php
+++ main.php
@@ -118,6 +118,6 @@ class UploadsSync
       "filenames" => $filenames
     );
 
-    $this->gearmanClient->doNormal("{$this->namespace}_delete", json_encode($data));
+    $this->gearmanClient->doBackground("{$this->namespace}_delete", json_encode($data));
   }
 }
